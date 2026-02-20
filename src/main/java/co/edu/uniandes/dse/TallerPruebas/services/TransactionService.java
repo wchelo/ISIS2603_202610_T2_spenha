531652cae32315c8b17203f6fe500d072f34fc38
wchelo <s.penha@uniandes.edu.co>
2026-02-20 17:11:08 -0500

    transaction service y tests

--- src/main/java/co/edu/uniandes/dse/TallerPruebas/services/TransactionService.java
+++ src/main/java/co/edu/uniandes/dse/TallerPruebas/services/TransactionService.java
@@ -1,5 +1,72 @@
 package co.edu.uniandes.dse.TallerPruebas.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.uniandes.dse.TallerPruebas.entities.AccountEntity;
+import co.edu.uniandes.dse.TallerPruebas.entities.TransactionEntity;
+import co.edu.uniandes.dse.TallerPruebas.exceptions.BusinessLogicException;
+import co.edu.uniandes.dse.TallerPruebas.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.TallerPruebas.repositories.AccountRepository;
+import co.edu.uniandes.dse.TallerPruebas.repositories.TransactionRepository;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
 public class TransactionService {
+    @Autowired
+    private AccountRepository accountRepository;  
+
+    @Autowired
+    private TransactionRepository transactionRepository;  
+
+
+
+    @Transactional
+    public TransactionEntity createTransaction(TransactionEntity transactionEntity) throws EntityNotFoundException, BusinessLogicException {
+        log.info("Inicia proceso de creación de la transacción");
+
+        if (transactionEntity.getMonto() <= 0) {
+            throw new BusinessLogicException("El monto de la transacción debe ser mayor a cero");
+        } 
+        return transactionRepository.save(transactionEntity);
+    }
+
+    @Transactional
+    public void moveCashBetweenAccounts(long OriginAccountId, long EndAccountId, double monto) throws EntityNotFoundException, BusinessLogicException {
+        log.info("Inicia proceso de movimiento de dinero entre cuentas con id = {} y {}", OriginAccountId, EndAccountId);
+        
+        // 1. Verificar que la cuenta origen existe
+        Optional<AccountEntity> FirstAccountEntity = accountRepository.findById(OriginAccountId);
+        if (FirstAccountEntity.isEmpty()) {
+            throw new EntityNotFoundException("La cuenta de origen no existe");
+        }
+
+        // 2. Verificar que la cuenta destino existe
+        Optional<AccountEntity> SecondAccountEntity = accountRepository.findById(EndAccountId);
+        if (SecondAccountEntity.isEmpty()) {
+            throw new EntityNotFoundException("La cuenta de destino no existe");
+        }
+
+        // 3. Verificar que la cuenta origen es distinta a la cuenta destino
+        if (OriginAccountId == EndAccountId) {
+            throw new BusinessLogicException("La cuenta de origen y destino son iguales");
+        }
+
+        // 4. Verificar que la cuenta origen tenga saldo suficiente
+        if (FirstAccountEntity.get().getSaldo() < monto) {
+            throw new BusinessLogicException("Saldo insuficiente en la cuenta de origen");
+        }
+
+        // 5. Actualizar el saldo de las dos cuentas
+        FirstAccountEntity.get().setSaldo(FirstAccountEntity.get().getSaldo() - monto);
+        SecondAccountEntity.get().setSaldo(SecondAccountEntity.get().getSaldo() + monto);
+        accountRepository.save(FirstAccountEntity.get());
+        accountRepository.save(SecondAccountEntity.get());
+    }
+
 
 }
--- src/test/java/co/edu/uniandes/dse/TallerPruebas/services/TransactionServiceTest.java
+++ src/test/java/co/edu/uniandes/dse/TallerPruebas/services/TransactionServiceTest.java
@@ -1,5 +1,123 @@
 package co.edu.uniandes.dse.TallerPruebas.services;
 
-public class TransactionServiceTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+
+import co.edu.uniandes.dse.TallerPruebas.entities.AccountEntity;
+import co.edu.uniandes.dse.TallerPruebas.entities.PocketEntity;
+import co.edu.uniandes.dse.TallerPruebas.exceptions.BusinessLogicException;
+import co.edu.uniandes.dse.TallerPruebas.exceptions.EntityNotFoundException;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+@DataJpaTest
+@Transactional
+@Import(TransactionService.class)
+public class TransactionServiceTest {
+    @Autowired
+    private TransactionService transactionService;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private PodamFactory factory = new PodamFactoryImpl();
+
+    private List<AccountEntity> accountList = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        clearData();
+        insertData();
+    }
+
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        entityManager.getEntityManager().createQuery("delete from PocketEntity").executeUpdate();
+        entityManager.getEntityManager().createQuery("delete from AccountEntity").executeUpdate();
+    }
+
+    /**
+     * Inserta datos iniciales para el correcto funcionamiento de las pruebas.
+     */
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            AccountEntity accountEntity = factory.manufacturePojo(AccountEntity.class);
+            accountEntity.setEstado("ACTIVA");
+            entityManager.persist(accountEntity);
+            accountList.add(accountEntity);
+        }
+
+    }
+
+    @Test
+    void testMoveCashBetweenAccounts() throws EntityNotFoundException, BusinessLogicException {
+        accountList.get(0).setSaldo(2000.0);
+        accountList.get(1).setSaldo(1000.0);
+        Double saldoA = accountList.get(0).getSaldo();
+        Double saldoB = accountList.get(1).getSaldo();
+
+        long firstId = accountList.get(0).getId();
+        long secondId = accountList.get(1).getId();
+
+        transactionService.moveCashBetweenAccounts(firstId, secondId, 1000.0);
+
+        assertEquals(saldoA - 1000.0, accountList.get(0).getSaldo());
+        assertEquals(saldoB + 1000.0, accountList.get(1).getSaldo());
+
+    }
+
+    @Test
+    void testMoveCashBetweenAccountsWithoutOrigin() {
+        long notId = 999L;
+        long Id = accountList.get(1).getId();
+
+        assertThrows(EntityNotFoundException.class, () -> {
+            transactionService.moveCashBetweenAccounts(notId, Id, 1000.0);
+        });
+    }
+
+    @Test
+    void testMoveCashBetweenAccountsWithoutEnd() {
+        long notId = 999L;
+        long Id = accountList.get(1).getId();
+
+        assertThrows(EntityNotFoundException.class, () -> {
+            transactionService.moveCashBetweenAccounts(Id, notId, 1000.0);
+        });
+    }
+
+    @Test
+    void testMoveCashBetweenAccountsWithSameAccount() {
+        long sameId = accountList.get(0).getId();
+
+        assertThrows(BusinessLogicException.class, () -> {
+            transactionService.moveCashBetweenAccounts(sameId, sameId, 1000.0);
+        });
+    }
+    @Test
+    void testMoveCashBetweenAccountsWithNotEnoughMoney() {
+        accountList.get(0).setSaldo(500.0);
+        long firstId = accountList.get(0).getId();
+        long secondId = accountList.get(1).getId();
+
+
+        assertThrows(BusinessLogicException.class, () -> {
+            transactionService.moveCashBetweenAccounts(firstId, secondId, 1000.0);
+        });
+
+    
+    }
 }
